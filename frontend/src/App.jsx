d7908ab0fd9647edd1c91b94bf58142f4eaab63d
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-25 13:15:04 -0300

    feat: configure routes with React Router in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="" element={< />} />
+      </Routes>
+    </Router>
   )
 }
 
