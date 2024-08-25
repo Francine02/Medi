640979c35c5d951b846ad3f5c3634484c599b915
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-25 16:54:30 -0300

    Feat: About.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { About } from "../components/home/About";
 import { Hero } from "../components/home/Hero";
 
 export function Home() {
     return (
-        <div>
+        <div className="bg-[#f0e9dd] min-h-screen">
             <Hero/>
+            <About/>
         </div>
     );
 }
