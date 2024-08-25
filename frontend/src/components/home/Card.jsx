b23c8c1f145748d492f0e6b93e4b6f42f1e71aea
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-25 18:16:17 -0300

    Feat: ScrollReveal in Home.jsx

--- frontend/src/components/home/Card.jsx
+++ frontend/src/components/home/Card.jsx
@@ -1,6 +1,6 @@
 export function Card({img, text}) {
     return (
-        <div className="bg-[#cddcd2] p-3 w-full sm:max-w-64 min-h-36 max-h-36">
+        <div className="bg-[#cddcd2] p-3 w-full sm:max-w-64 min-h-40 max-h-40">
             <img src={img} alt="icon" className="w-10 mx-auto" />
             <p className="font-medium pt-2 leading-relaxed text-base lg:text-lg text-center">{text}</p>
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,19 +2,39 @@ import { About } from "../components/home/About";
 import { Card } from "../components/home/Card";
 import { Footer } from "../components/home/Footer";
 import { Hero } from "../components/home/Hero";
+import ScrollReveal from "scrollreveal";
+import { useEffect } from "react";
 
 export function Home() {
+    useEffect(() => { //Para a configuração do ScrollReveal
+        const sr = ScrollReveal()
+
+        sr.reveal('.one', {
+            duration: 950,
+            origin: 'left',
+            distance: '100px',
+            easing: 'ease-in-out',
+        })
+
+        sr.reveal('.two', {
+            duration: 950,
+            origin: 'right',
+            distance: '100px',
+            easing: 'ease-in-out',
+        })
+
+    }, [])
     return (
-        <div className="bg-[#f0e9dd] min-h-screen">
-            <Hero/>
-            <About/>
-            <div className="grid grid-cols-1 sm:grid-cols-3 px-7 sm:px-12 lg:px-20 gap-4 justify-items-center">
-                <Card img="https://img.icons8.com/?size=100&id=34998&format=png&color=000000" text="Adicionar e Gerenciar Medicamentos"/>
-                <Card img="https://img.icons8.com/?size=100&id=40401&format=png&color=000000" text="Receber Notificações por Email"/>
-                <Card img="https://img.icons8.com/?size=100&id=10082&format=png&color=000000" text="Visualizar Seu Cronograma de Medicamentos"/>
+        <div className="bg-[#f0e9dd] min-h-screen overflow-auto">
+            <div className="one"><Hero /></div>
+            <div className="two"><About /></div>
+            <div className="grid grid-cols-1 sm:grid-cols-3 px-7 sm:px-12 lg:px-20 gap-4 justify-items-center one">
+                <Card img="https://img.icons8.com/?size=100&id=34998&format=png&color=000000" text="Adicionar e Gerenciar Medicamentos" />
+                <Card img="https://img.icons8.com/?size=100&id=40401&format=png&color=000000" text="Receber Notificações por Email" />
+                <Card img="https://img.icons8.com/?size=100&id=10082&format=png&color=000000" text="Visualizar Seu Cronograma de Medicamentos" />
             </div>
 
-            <Footer/>
+            <div className="two"><Footer /></div>
         </div>
     )
 }
