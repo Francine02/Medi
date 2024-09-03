ef4a27aa497694b31f2f36c3d8d7b9cdd8f1aa82
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-03 14:05:02 -0300

    Feat: DTO of medicine

---
+++ backend/src/main/java/com/me/backend/dtos/MedicioneDTO.java
@@ -0,0 +1,14 @@
+package com.me.backend.dtos;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record MedicioneDTO(
+    Long id,
+    @NotBlank(message = "Nome não pode ser nulo")
+    String name,
+    String description,
+    @NotBlank(message = "Data não pode ser nulo")
+    LocalDateTime dateTime
+) {}
