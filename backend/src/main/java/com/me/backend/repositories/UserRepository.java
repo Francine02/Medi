27d65820741e2cc23f9dd2e4070531601a746ae4
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-22 15:23:39 -0300

    feat: method to find by email

--- backend/src/main/java/com/me/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/me/backend/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.me.backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.me.backend.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<UserDetails> findByEmail(String email);
 }
