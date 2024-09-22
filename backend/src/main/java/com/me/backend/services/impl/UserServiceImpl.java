37cfee33d06215c5d3c88a20e03bed698d7871e1
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-09-22 17:27:53 -0300

    feat: UserServiceImpl with method loadUserByUsername - findByEmail

---
+++ backend/src/main/java/com/me/backend/services/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.me.backend.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.me.backend.entities.User;
+import com.me.backend.repositories.UserRepository;
+
+@Component
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return user;
+    }
+
+}
