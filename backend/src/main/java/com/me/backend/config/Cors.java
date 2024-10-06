33aed0f7ccbb4cc5581b9f6333ef545b597d23e3
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-06 17:20:46 -0300

    feat: links in cors

--- backend/src/main/java/com/me/backend/config/Cors.java
+++ backend/src/main/java/com/me/backend/config/Cors.java
@@ -9,7 +9,10 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "https://medi-f3fk.onrender.com",
+                        "https://medi-five-fawn.vercel.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
