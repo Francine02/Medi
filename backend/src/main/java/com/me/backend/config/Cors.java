89107651b1abb34743360db286ea8b733559956c
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-06 17:16:25 -0300

    feat: add Docker and Docker Compose configuration

--- backend/src/main/java/com/me/backend/config/Cors.java
+++ backend/src/main/java/com/me/backend/config/Cors.java
@@ -5,9 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Cors implements WebMvcConfigurer{
+public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
