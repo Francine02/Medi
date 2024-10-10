b287e5fc80d7e884dd5e71de8e33e49e5d3d0722
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-10 20:13:17 -0300

    feat: add query to find medicines between dates with unsent emails

--- backend/src/main/java/com/me/backend/repositories/MedicineRepository.java
+++ backend/src/main/java/com/me/backend/repositories/MedicineRepository.java
@@ -1,9 +1,12 @@
 package com.me.backend.repositories;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.me.backend.entities.Medicine;
 import com.me.backend.entities.User;
@@ -11,4 +14,7 @@ import com.me.backend.entities.User;
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
     Optional <Medicine> findByName(String name);
     List<Medicine> findByUser(User user);
+    @Query("SELECT m FROM Medicine m WHERE m.dateTime BETWEEN :start AND :end AND m.emailSent = false")
+    List<Medicine> findByDateTimeBetweenAndEmailSentFalse(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
 }
