a210a88de7096953712bd70157ef821e97736ae7
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-11 16:16:53 -0300

    feat: configure route authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
+import { Medicines } from './pages/Medicines'
 
 function App() {
-
+  const isAuthenticated = !!localStorage.getItem('token');
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<Home/>} />
-        <Route path="/auth/login" element={<Login/>} />
-        <Route path="/auth/register" element={<Register/>} />
+        <Route path="*" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route
+          path="/medicines"
+          element={isAuthenticated ? <Medicines /> : <Navigate to="/auth/login" />} />
       </Routes>
     </Router>
   )
