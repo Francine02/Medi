4d8c51e9a37b6620757cbbe980a8a9ae8d4de5c5
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 16:08:05 -0300

    feat: data formatting for better display

--- frontend/src/components/medicines/TableContent.jsx
+++ frontend/src/components/medicines/TableContent.jsx
@@ -1,18 +1,27 @@
-export function TableContent({name, hour, description, id}) {
+import { useMedicineContext } from "../../context/MedicineContext"
+
+export function TableContent({ name, hour, description, medicine }) {
+    const { openEdit } = useMedicineContext()
+
+    const formatForDisplay = (dateTime) => { // Formatar data exibida
+        const date = new Date(dateTime)
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+    }
+
     return (
         <>
-            <th scope="row" className="px-6 py-4 font-medium ">
+            <th scope="row" className="px-6 py-4 font-medium max-w-32">
                 {name}
             </th>
             <td className="px-6 py-4">
-                {hour}
+                {formatForDisplay(hour)}
             </td>
             <td className="px-6 py-4 max-w-24">
                 {description}
             </td>
 
             <td className="px-6 py-4">
-                <a href="#" className="font-medium text-[#8f5cd7] hover:underline">Edit</a>
+                <a onClick={() => openEdit(medicine)} className="font-medium text-[#8f5cd7] hover:underline cursor-pointer">Edit</a>
             </td>
         </>
     )
