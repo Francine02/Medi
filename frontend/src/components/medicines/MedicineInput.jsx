80d2ba024127242e52b33ab760e7c5e2baea8422
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 16:10:03 -0300

    feat: field to enter the email to send a reminder

--- frontend/src/components/medicines/MedicineInput.jsx
+++ frontend/src/components/medicines/MedicineInput.jsx
@@ -1,8 +1,11 @@
-export function MedicineInput() {
+export function MedicineInput({ data, onChange }) {
     return (
         <div className="flex flex-col space-y-5">
             <label className="block">
                 <input
+                    value={data.name}
+                    onChange={onChange}
+                    name="name"
                     type="text"
                     placeholder="Nome do medicamento"
                     className="w-full sm:w-96 p-3 sm:p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#4c0192] mt-2"
@@ -10,7 +13,21 @@ export function MedicineInput() {
             </label>
 
             <label className="block">
+                <input
+                    value={data.emailToSend}
+                    onChange={onChange}
+                    name="emailToSend"
+                    type="email"
+                    placeholder="E-mail para enviar lembrete"
+                    className="w-full sm:w-96 p-3 sm:p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#4c0192] mt-2"
+                />
+            </label>
+
+            <label className="block">
                 <textarea
+                    value={data.description}
+                    onChange={onChange}
+                    name="description"
                     placeholder="Descrição do medicamento"
                     className="w-full sm:w-96 p-3 sm:p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#4c0192] max-h-28 resize-none mt-2"
                     style={{ maxHeight: '112px' }}
@@ -20,6 +37,9 @@ export function MedicineInput() {
             <label className="block">
                 <span className="text-lg sm:text-xl text-gray-800">Data e horário</span>
                 <input
+                    name="dateTime"
+                    value={data.dateTime}
+                    onChange={onChange}
                     type="datetime-local"
                     className="w-full sm:w-96 p-3 sm:p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#4c0192] mt-2"
                 />
