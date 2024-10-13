ea31c4d0907695a9ebd8ba8653ef292439ae106c
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 15:57:33 -0300

    feat: requests to create, edit and delete medications

--- frontend/src/utils/medicine.js
+++ frontend/src/utils/medicine.js
@@ -19,3 +19,33 @@ export const getMedicines = async () => {
         console.log("Ocorreu um erro ao pegar todos os medicamentos!")
     }
 }
+
+export const postMedicine = async (data) => {
+    try {
+        const response = await axios.post(`${API}/medi/create`, data, config)
+        console.log("Sucesso ao criar medicamento!");
+        return response.data
+    } catch (error) {
+        console.log("Ocorreu um erro ao ao criar medicamento!")
+    }
+}
+
+export const updateMedicine = async (id, data) => {
+    try {
+        const response = await axios.put(`${API}/medi/${id}`, data, config)
+        console.log("Sucesso ao atualizar medicamento!");
+        return response.data
+    } catch (error) {
+        console.log("Ocorreu um erro ao ao atualizar medicamento!")
+    }
+}
+
+export const deleteMedicine = async (id) => {
+    try {
+        const response = await axios.delete(`${API}/medi/${id}`, config)
+        console.log("Sucesso ao excluir medicamento!");
+        return response.data
+    } catch (error) {
+        console.log("Ocorreu um erro ao ao excluir medicamento!")
+    }
+}
