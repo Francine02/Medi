f9132fa9803a201ac5f4ef1125541fde10a50b73
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 16:04:49 -0300

    feat: trigger openCreate using MedicineContext

--- frontend/src/components/medicines/Button.jsx
+++ frontend/src/components/medicines/Button.jsx
@@ -1,6 +1,10 @@
+import { useMedicineContext } from "../../context/MedicineContext"
+
 export function Button({type}) {
+    const { openCreate } = useMedicineContext()
+    
     return (
-        <div type={type} id='button' className="bg-[#8f5cd7] hover:opacity-95 p-2 w-14 h-14 2xl:w-16 2xl:h-16 rounded-full fixed bottom-6 right-6 sm:right-9 lg:right-16 2xl:right-28 2xl:bottom-9 cursor-pointer shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" >
+        <div onClick={openCreate} type={type} id='button' className="bg-[#8f5cd7] hover:opacity-95 p-2 w-14 h-14 2xl:w-16 2xl:h-16 rounded-full fixed bottom-6 right-6 sm:right-9 lg:right-16 2xl:right-28 2xl:bottom-9 cursor-pointer shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" >
             <img src="https://img.icons8.com/?size=100&id=37784&format=png&color=FFFFFF" alt="adicionar" />
         </div>
     )
