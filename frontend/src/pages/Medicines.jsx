309d5eae0a9b9ff80c89a9e5fd292f49a86b9303
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 16:11:13 -0300

    feat: integrate MedicineContext to manage modal visibility and editing state

--- frontend/src/pages/Medicines.jsx
+++ frontend/src/pages/Medicines.jsx
@@ -2,16 +2,24 @@ import bgImage from '../assets/5.png';
 import { Button } from '../components/medicines/Button';
 import { MedicineConfig } from '../components/medicines/MedicineConfig';
 import { Table } from '../components/medicines/Table';
+import { useMedicineContext } from "../context/MedicineContext";
 
 export function Medicines() {
+    const { showConfig, closeConfig, isEditing } = useMedicineContext()
+
     return (
-        <div className="px-7 sm:px-12 lg:px-20 2xl:px-48 py-10 bg-[#f0e9dd] h-full" style={{ backgroundImage: `url(${bgImage})` }}>
+        <div className="px-7 sm:px-12 lg:px-20 2xl:px-48 py-10 bg-[#f0e9dd] bg-cover h-screen" style={{ backgroundImage: `url(${bgImage})` }}>
             <h1 className="text-[28px] sm:text-4xl 2xl:text-5xl text-center font-black mb-4 text-[#8f5cd7] sm:pt-10">Medicamentos</h1>
             <Table />
 
-            {/* <div className="fixed inset-0 flex items-center justify-center">
-                <MedicineConfig />
-            </div> */}
+            {showConfig && (
+                <div className="fixed inset-0 flex items-center justify-center">
+                    <MedicineConfig
+                        title={isEditing ? "Editar Medicamento" : "Novo Medicamento"}
+                        onClose={closeConfig}
+                    />
+                </div>
+            )}
 
             <Button />
         </div>
