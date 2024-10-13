0f2cbc783c442736d0e6550ebee60eddd3162dd3
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-13 16:03:00 -0300

    feat: wrap application with MedicineProvider for global state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { MedicineProvider } from './context/MedicineContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MedicineProvider>
+      <App />
+    </MedicineProvider>
 )
